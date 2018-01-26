d4a1044edbb827e90814533d5caa5c92a45b4dd8
Yang <zhy0216@gmail.com>
2018-01-26 00:24:08 -0800

    add flex css layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+class AppInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: "",
+            extra: "||"
+        };
+    }
+
+
+    render(){
+      return <textarea className="App-input" onChange={() => this.setState({data: this.state.data + "ss"})} value={this.state.data + this.state.extra}></textarea>;
+    }
+}
+
+class AppOutput extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return <div className="App-output">First pass the data from the child to the parent, as an argument into a callback from the parent. Set this incoming parameter as a state on the parent component, then pass it as a prop to the other child (see above example). The sibling can
+        </div>;
+    }
+
+}
+
 class App extends Component {
   render() {
     return (
@@ -8,9 +36,10 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Easy Code Generator</h1>
         </header>
-          <textarea className="App-input"></textarea>
-          <div className="App-output">
-          </div>
+        <div className="App-body">
+          <AppInput />
+          <AppOutput />
+        </div>
       </div>
     );
   }
--- src/util.js
+++ src/util.js
@@ -101,3 +101,19 @@ export function generateTypes(data, className){
     generateHelper(data, className, stack);
     return stack;
 }
+
+// https://davidwalsh.name/javascript-debounce-function
+export function debounce(func, wait, immediate) {
+	let timeout;
+	return function() {
+		let context = this, args = arguments;
+		let later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		let callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
